1a674245513732e2b57b05b212bd07cadd425ab3
Jorge Escalante <hi@jescalante.me>
2023-08-29 12:20:42 -0600

    Abstracción y encapsulamiento

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from datetime import date
 from cat import Cat
 from food import Food
+from math import sqrt
 
 
 def main():
+    # 1 - Identidad
     cat1 = Cat('Gardfield', 5, 'Anaranjado')
-    cat2 = Cat('Luna', 1, 'Gris')
+    cat2 = Cat('Luna', 2, 'Café')
+    cat3 = Cat('Misho', 1, 'Negro')
 
-    tuna = Food('Atún', 10, date(2023, 9, 22))
+    print(id(cat1))
+    print(id(cat2))
+    print(id(cat3))
 
+    # 2 - Estado
+    print('Nombre:', cat1.name)
+    print('Edad:', cat1.age)
+
+    cat1.name = 'Señor Gato'
+    cat1.age = 1
+
+    print('Muchas líneas de código...')
+
+    print('Nombre:', cat1.name)
+    print('Edad:', cat1.age)
+
+    # 3 - Comportamiento
+    print('Energía al iniciar:', cat1.energy)
     cat1.run()
-    cat1.run()
+    print('Energía al finalizar:', cat1.energy)
 
-    cat2.run()
-
-    print(cat1)
-    cat1.eat(tuna)
-    print(cat1)
-
-    print('Edad en main:', cat1.age)
-    cat1.age = -20
-    print('Nueva edad:', cat1.age)
+    n = int(input('Ingrese un número: '))
+    resultado = sqrt(n)
+    print('El resultado es:', resultado)
 
 
 main()
